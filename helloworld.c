23299cdebcfa901cdd4f1ac68def2d3a45c04400
AsaAdomatis <90803033+AsaAdomatis@users.noreply.github.com>
2022-09-09 11:07:23 -0400

    Updated helloworld.c
    Refactored the printf messages into a function.

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main() {
+// Descr: Prints greetings to sdio
+void messages()
+{ 
   printf("Hello World!\n");
   printf("Tgif\n");
+}
+
+int main() {
+  messages();
   return 0;
 }
